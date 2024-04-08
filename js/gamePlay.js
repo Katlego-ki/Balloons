d7e01178701e81f7cc74cf9fb8671ce8b4b5fe50
Katlego-ki <katlegobairvin@gmail.com>
2024-04-08 21:22:41 +0200

    added container for balloons

--- js/gamePlay.js
+++ js/gamePlay.js
@@ -1,1 +1,32 @@
 //Use React!! Because it's easy
+
+let container = document.querySelector('.container');
+
+//To select mode easy:id = 1, medium: id = 2, hard: id = 3
+let selectMode = () => {
+    
+    let chooseMode = document.createElement('div');
+
+    for(let i = 1; i <= 3; i++){
+        container.appendChild(chooseMode);
+        chooseMode.id = i;
+        chooseMode.setAttribute('onclick','balloonsFiled(this.id)' )
+    }
+}
+
+
+//To Paint gray Balloons
+
+let balloonsField = (mode) => {
+
+    
+
+}
+
+
+/*
+const newImg = document.createElement("img");
+    newImg.classList.add('cards'); //??
+    newImg.id = i;
+    newImg.src = "images/Card-Pictures/Unturned.png";
+    cardsGrid.appendChild(newImg);
